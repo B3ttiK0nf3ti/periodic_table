a6c0317a34bd68db347fc933969bbb370e24dd7d
Bettina Weigandt <weigandt.bettina@gmail.com>
2025-05-23 09:32:10 +0000

    feat: adding search kind

--- element.sh
+++ element.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
-echo "Please provide an element as an argument."
+# if no arguement is provided
+if [[ -z $1 ]];
+then
+    echo  "Please provide an element as an argument."
+    exit
+fi
+
+# check how to search
+if [[ $1 =~  ^[0-9]+$ ]]
+then 
+  SEARCH_BY="atomitc_number"
+elif [[ $1 =~  ^[A-Z][a-z]?$ ]]
+then
+  SEARCH_BY="symbol"
+else
+  SEARCH_BY="name"
+fi
+
