1f6f48a0f9b54fb5a1587b4828ffeed6c7f4de0b
Bettina Weigandt <weigandt.bettina@gmail.com>
2025-05-23 09:55:52 +0000

    fix: changing database connection, rename atomic_number in SEARCH_BY

--- element.sh
+++ element.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql  --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 # if no arguement is provided
 if [[ -z $1 ]];
@@ -11,7 +11,7 @@ fi
 # check how to search
 if [[ $1 =~  ^[0-9]+$ ]]
 then 
-  SEARCH_BY="atomitc_number"
+  SEARCH_BY="atomic_number"
 elif [[ $1 =~  ^[A-Z][a-z]?$ ]]
 then
   SEARCH_BY="symbol"
@@ -19,5 +19,5 @@ else
   SEARCH_BY="name"
 fi
 
-RESULT=$($PSQL "SELECT atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name FROM properties INNER JOIN elements USING(atomic_number)")
+RESULT=$($PSQL "SELECT p.atomic_number, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, e.symbol, e.name, t.type FROM properties p INNER JOIN elements e USING(atomic_number) INNER JOIN types t USING(type_id) WHERE $SEARCH_BY = '$1'")
 
